a46a526ff892c46c7bd42d488988d8e4511abc68
saifulislam68 <saiful15-13285@diu.edu.bd>
2022-02-04 22:32:58 +0600

    rate my work

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:myshop/providers/auth.dart';
+import './screens/products_overview_screen.dart';
+import './screens/splash_screen.dart';
+import 'package:provider/provider.dart';
+
+import './screens/cart_screen.dart';
+
+import './screens/product_detail_screen.dart';
+import './providers/products.dart';
+import './providers/cart.dart';
+import './providers/orders.dart';
+import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Auth()
+          ),
+        // ignore: missing_required_param
+        ChangeNotifierProxyProvider <Auth,Products> (
+          update:(ctx,auth,previousProducts) => Products(
+            auth.token,
+            auth.userId,
+          previousProducts ==null?[]: previousProducts.items),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+        // ignore: missing_required_param
+        ChangeNotifierProxyProvider<Auth, Orders>(
+          update: (ctx, auth, previousOrders) => Orders(
+            auth.token,
+            auth.userId,
+              previousOrders == null ? [] : previousOrders.orders),
+        ),
+      ],
+      child: Consumer<Auth>(builder:(ctx,auth,_)=> MaterialApp(
+            title: 'MyShop',
+            theme: ThemeData(
+              fontFamily: 'Lato',
+              colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                  .copyWith(secondary: Colors.deepOrange),
+            ),
+            home:auth.isAuth ? ProductsOverviewScreen() : FutureBuilder(
+              future: auth.tryAutoLogin(),
+              builder: (ctx,authResultSnapshot)=>authResultSnapshot.connectionState==ConnectionState.waiting ? 
+              SplashScreen(): AuthScreen(),),
+            routes: {
+              ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+              CartScreen.routeName: (ctx) => CartScreen(),
+              OrdersScreen.routeName: (ctx) => OrdersScreen(),
+              UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+              EditProductScreen.routeName: (ctx) => EditProductScreen(),
+            },),),
+    );
+  }
+}
